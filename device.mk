23e83fca370c9d4b8b93667c9265ca14744258e8
devil-black-786 <fahimahmad904@gmail.com>
2021-03-23 09:00:39 +0530

    CPH1859: Remove Treble app
    *We no longer use it, So there is no point in having it.

--- device.mk
+++ device.mk
@@ -168,9 +168,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ImsInit
 
-PRODUCT_PACKAGES += \
-    TrebleApp
-
 PRODUCT_COPY_FILES += \
        $(LOCAL_PATH)/permissions/interfaces.xml:system/etc/permissions/interfaces.xml
 
